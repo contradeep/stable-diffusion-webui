13930cf7856ab68916c0d18666403564847e47e9
contradeep <167467708+contradeep@users.noreply.github.com>
2024-04-25 21:45:08 -0400

    Update prov.sh

--- config/provisioning/prov.sh
+++ config/provisioning/prov.sh
@@ -47,6 +47,8 @@ LORA_MODELS=(
     "https://civitai.com/api/download/models/244808"
     "https://civitai.com/api/download/models/354128"
     "https://civitai.com/api/download/models/330475"
+    "https://civitai.com/api/download/models/398847"
+    "https://civitai.com/api/download/models/382152"
 )
 
 VAE_MODELS=(
